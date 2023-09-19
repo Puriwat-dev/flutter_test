8c8b158c7617cd4e008103d9e3cb5d76b25ccf3a
Puriwat-dev <puriwatdev@gmail.com>
2023-09-19 17:50:28 +0700

    for O

--- lib/components/bottom_navigation_bar.dart
+++ lib/components/bottom_navigation_bar.dart
@@ -23,24 +23,24 @@ class BottomNavState extends State<BottomNav> {
       selectedIndex: currentPageIndex,
       destinations: const <Widget>[
         NavigationDestination(
-          selectedIcon: Icon(Icons.map, color: Colors.blue),
+          selectedIcon: Icon(Icons.map, color: Color(0xff2B91BD)),
           icon: Icon(
             Icons.map,
           ),
           label: 'Map',
         ),
         NavigationDestination(
-          selectedIcon: Icon(Icons.warning, color: Colors.blue),
+          selectedIcon: Icon(Icons.warning, color: Color(0xff2B91BD)),
           icon: Icon(Icons.warning),
           label: 'Report',
         ),
         NavigationDestination(
-          selectedIcon: Icon(Icons.notifications, color: Colors.blue),
+          selectedIcon: Icon(Icons.notifications, color: Color(0xff2B91BD)),
           icon: Icon(Icons.notifications),
           label: 'Notifications',
         ),
         NavigationDestination(
-          selectedIcon: Icon(Icons.settings, color: Colors.blue),
+          selectedIcon: Icon(Icons.settings, color: Color(0xff2B91BD)),
           icon: Icon(Icons.settings),
           label: 'Setting',
         ),
