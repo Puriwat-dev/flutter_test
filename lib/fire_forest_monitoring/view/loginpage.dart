b69be0e09ae892444e9468d364540c43fb00f6d0
Asuka81 <wongsapat.dev@gmail.com>
2023-09-21 17:51:12 +0700

    Add Country Code Selector

--- lib/fire_forest_monitoring/view/loginpage.dart
+++ lib/fire_forest_monitoring/view/loginpage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:country_code_picker/country_code_picker.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -16,10 +16,7 @@ class LoginPage extends StatelessWidget {
       child: const Scaffold(
         backgroundColor: Colors.transparent,
         body: Column(
-          children: <Widget>[
-            Logo(),
-            LoginForm(),
-          ],
+          children: <Widget>[Logo(), LoginForm(), SwitchAccountWidget()],
         ),
       ),
     );
@@ -55,70 +52,142 @@ class LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: 380,
-        height: 240,
-        child: Opacity(
-            opacity: 1,
-            child: Card(
-                clipBehavior: Clip.antiAlias,
-                semanticContainer: true,
-                color: Color.fromARGB(90, 9, 21, 34),
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(width: 1),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsetsDirectional.only(top: 20),
-                      child: Text(
-                        'Sign In',
-                        style: TextStyle(
-                            fontSize: 32,
-                            color: Colors.white,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.left,
-                      ),
+      width: 380,
+      height: 240,
+      child: Opacity(
+          opacity: 1,
+          child: Card(
+              clipBehavior: Clip.antiAlias,
+              semanticContainer: true,
+              color: const Color.fromARGB(90, 9, 21, 34),
+              shadowColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(width: 1),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsetsDirectional.only(top: 20),
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                          fontSize: 32,
+                          color: Colors.white,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.left,
                     ),
-                    Padding(
-                        padding: const EdgeInsetsDirectional.only(top: 20),
-                        child: SizedBox(
+                  ),
+                  Padding(
+                      padding: const EdgeInsetsDirectional.only(top: 20),
+                      child: SizedBox(
                           width: 300,
                           height: 50,
-                          child: TextField(
-                            decoration: InputDecoration(
-                              enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(
-                                      44), // Change this value
-                                  borderSide: const BorderSide(
-                                      width: 1, color: Colors.white)),
-                              focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(
-                                      44), // Change this value
-                                  borderSide: const BorderSide(
-                                      width: 1, color: Colors.white)),
-                              fillColor: const Color(0xff091522),
-                              filled: true,
-                            ),
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        )),
-                    const Padding(
-                        padding: EdgeInsetsDirectional.only(top: 20),
-                        child: SizedBox(
-                            width: 300,
-                            height: 50,
-                            child: FilledButton(
-                                onPressed: null,
-                                style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Color(0xffEF5350))),
-                                child: Text(
-                                  "Save",
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20),
-                                )))),
-                  ],
-                ))));
+                          child: Row(children: <Widget>[
+                            const Expanded(
+                              flex:2,
+                                child: CountryCodePicker(
+                              onChanged: print,
+                              initialSelection: 'TH',
+                              favorite: ['+66', 'TH'],
+                              showCountryOnly: true,
+                              showOnlyCountryWhenClosed: false,
+                              textStyle: TextStyle(color: Colors.white),
+                              flagWidth: 20,
+                              showFlag: false,
+                              showFlagDialog: true,
+                            )),
+                            Expanded(
+                              flex: 4,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(
+                                          44), // Change this value
+                                      borderSide: const BorderSide(
+                                          width: 1, color: Colors.white)),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(
+                                          44), // Change this value
+                                      borderSide: const BorderSide(
+                                          width: 1, color: Colors.white)),
+                                  fillColor: const Color(0xff091522),
+                                  filled: true,
+                                ),
+                                
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                            )
+                          ]))),
+                  Padding(
+                      padding: EdgeInsetsDirectional.only(top: 20),
+                      child: SizedBox(
+                          width: 300,
+                          height: 50,
+                          child: FilledButton(
+                              onPressed: null,
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStatePropertyAll(
+                                      Color(0xffEF5350))),
+                              child: Text(
+                                "Send",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              )))),
+                ],
+              ))),
+    );
+  }
+}
+
+class SwitchAccount extends StatelessWidget {
+  const SwitchAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 200,
+        height: 100,
+        constraints: const BoxConstraints(maxHeight: 200),
+        child: const Align(
+            alignment: Alignment.bottomRight,
+            child: Row(children: [
+              Text(
+                'Switch Account',
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                  padding: EdgeInsets.only(left: 5),
+                  child: Icon(
+                    Icons.account_circle,
+                    color: Colors.white,
+                    size: 20,
+                  ))
+            ])));
+  }
+}
+
+class SwitchAccountWidget extends StatelessWidget {
+  const SwitchAccountWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Align(
+      alignment: Alignment.bottomRight, // Align to the bottom left
+      child: Padding(
+        padding: EdgeInsets.only(
+          top: 230,
+          left: 270,
+        ), // Adjust padding as needed
+        child: SwitchAccount(),
+      ),
+    );
   }
 }
 
